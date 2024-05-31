f28e823c2c5d476cdfe063be86d5fff20b6fe50c
Christopher Stocker <julienstocker@icloud.com>
2024-05-31 14:11:15 +0200

    icons

--- config/config.js
+++ config/config.js
@@ -1,25 +1,16 @@
 let config = {
-	address: "localhost",	// Address to listen on, can be:
-							// - "localhost", "127.0.0.1", "::1" to listen on loopback interface
-							// - another specific IPv4/6 to listen on a specific interface
-							// - "0.0.0.0", "::" to listen on any interface
-							// Default, when address config is left out or empty, is "localhost"
+	address: "localhost",
 	port: 8080,
-	basePath: "/",	// The URL path where MagicMirror² is hosted. If you are using a Reverse proxy
-									// you must set the sub path here. basePath must end with a /
-	ipWhitelist: [],	// Set [] to allow all IP addresses
-									// or add a specific IPv4 of 192.168.1.5 :
-									// ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.1.5"],
-									// or IPv4 range of 192.168.3.0 --> 192.168.3.15 use CIDR format :
-									// ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.3.0/28"],
+	basePath: "/",
+	ipWhitelist: [],
 
-	useHttps: false,			// Support HTTPS or not, default "false" will use HTTP
-	httpsPrivateKey: "",	// HTTPS private key path, only require when useHttps is true
-	httpsCertificate: "",	// HTTPS Certificate path, only require when useHttps is true
+	useHttps: false,
+	httpsPrivateKey: "",
+	httpsCertificate: "",
 
 	language: "en",
 	locale: "en-US",
-	logLevel: ["INFO", "LOG", "WARN", "ERROR"], // Add "DEBUG" for even more logging
+	logLevel: ["INFO", "LOG", "WARN", "ERROR"],
 	timeFormat: 24,
 	units: "metric",
 
@@ -29,34 +20,27 @@ let config = {
 			position: "top_center",
 			config: {
 				totalYears: 125,
-				birthdate: "1996-11-14", // error on 2019,2013, In a standard year: 365 \div 7 \approx 52.142857365÷7≈52.142857 weeks. In a leap year: 366 \div 7 \approx 52.285714366÷7≈52.285714 weeks.
-    			
+				birthdate: "1996-11-14",
 				duration_events: [
-					{ event_text: "Birth", duration_years: 0 , duration_weeks: 0},
-					{ event_text: "Early Years", duration_years: 7 , duration_weeks: 0},
-					{ event_text: "Elementary", duration_years: 6 , duration_weeks: 0},
+					{ event_text: "Birth", duration_years: 0, duration_weeks: 0 },
+					{ event_text: "Early Years", duration_years: 7, duration_weeks: 0 },
+					{ event_text: "Elementary", duration_years: 6, duration_weeks: 0 },
 					{ event_text: "Middle School", duration_years: 2, duration_weeks: 0 },
 					{ event_text: "High School", duration_years: 4, duration_weeks: 0 },
-					{ event_text: "College", duration_years: 7 , duration_weeks: 0},
-					{ event_text: "Career", duration_years: 40, duration_weeks: 0},
-					{ event_text: "Retirement", duration_years: 20, duration_weeks: 0}
-				  ],
-
-				date_events: [
-					{ event_text: "Birth", date: "1996-11-14" , duration_weeks: 0},
-					{ event_text: "Graduated High School", date: "2015-06-20", duration_weeks: 0 },
-					{ event_text: "Submited Thesis", date: "2023-12-11", duration_weeks: 0 },
-					{ event_text: "Started Career", date: "2024-06-01", duration_weeks: 0 },
-					{ event_text: "Retirement", date: "2056-06-01",	duration_weeks: 0 },
-					{ event_text: "Body Ratio 1.6", date: "2024-12-12",	duration_weeks: 0 },
-					{ event_text: "Marathon", date: "2026-12-12",	duration_weeks: 0 },
-					{ event_text: "Triathlon", date: "2028-12-12",	duration_weeks: 0 },
-					{ event_text: "Swiss Male Life Expectancy", date: "2079-07-02",	duration_weeks: 0 },
+					{ event_text: "College", duration_years: 7, duration_weeks: 0 },
+					{ event_text: "Career", duration_years: 40, duration_weeks: 0 },
+					{ event_text: "Retirement", duration_years: 20, duration_weeks: 0 }
 				],
-
-			  
+				date_events: [
+					{ event_text: "Birth", date: "1996-11-14", duration_weeks: 0, icon_name: "heart.svg" },
+					{ event_text: "Graduated High School", date: "2015-06-20", duration_weeks: 0, icon_name: "trophy2.svg" },
+					{ event_text: "Submitted Thesis", date: "2023-12-11", duration_weeks: 0, icon_name: "icon.svg" },
+					{ event_text: "Started Career", date: "2024-06-01", duration_weeks: 0, icon_name: "shoe.svg" },
+					{ event_text: "Retirement", date: "2056-06-01", duration_weeks: 0, icon_name: "trophy.svg" },
+					{ event_text: "Swiss Male Life Expectancy", date: "2079-07-02", duration_weeks: 0, icon_name: "icon.svg" }
+				]
 			}
-		}		
+		}
 	]
 };
 
--- modules/MMM-LifeCalendar/MMM-LifeCalendar.js
+++ modules/MMM-LifeCalendar/MMM-LifeCalendar.js
@@ -1,9 +1,10 @@
 Module.register("MMM-LifeCalendar", {
   defaults: {
     weeksPerYear: 52,
-    totalYears: 125,
+    totalYears: 120,
     birthdate: "2000-01-01",
-    colorScheme: "gray", // Add this line for color scheme
+    colorScheme: "green", // Color Schemes: {default, gray, green, neutral}
+    icons_display: false,
     duration_events: [
       { event_text: "Birth", duration_years: 0, duration_weeks: 0 },
       { event_text: "Early Years", duration_years: 6, duration_weeks: 0 },
@@ -12,14 +13,6 @@ Module.register("MMM-LifeCalendar", {
       { event_text: "High School", duration_years: 4, duration_weeks: 0 },
       { event_text: "College", duration_years: 7, duration_weeks: 0 },
       { event_text: "Career", duration_years: 40, duration_weeks: 0 },
-      { event_text: "Retirement", duration_years: 20, duration_weeks: 0}
-    ],
-    date_events: [
-      { event_text: "Birth", date: "1996-11-14", duration_weeks: 0 },
-      { event_text: "Graduated High School", date: "2015-06-20", duration_weeks: 0 },
-      { event_text: "Submitted Thesis", date: "2023-12-11", duration_weeks: 0 },
-      { event_text: "Started Career", date: "2024-06-01", duration_weeks: 0 },
-      { event_text: "Retirement", date: "2056-06-01", duration_weeks: 0 },
     ],
   },
 
@@ -56,15 +49,12 @@ Module.register("MMM-LifeCalendar", {
   getDom: function () {
     const wrapper = document.createElement("div");
     wrapper.className = "bright medium";
-
-    const ageText = document.createElement("div");
-    ageText.innerHTML = "You are " + this.getAge() + " weeks old.";
-    wrapper.appendChild(ageText);
-    const weeksLived = this.getAge();
-
+  
+    const weeksLived = this.getAge(); // Retain this line to keep the functionality
+  
     // Create a moment object for the date of birth
     var dob = moment(this.config.birthdate);
-
+  
     // Loop through each event in the date_events array
     for (let i = 0; i < this.config.date_events.length; i++) {
       // Create a moment object for the event date
@@ -72,24 +62,24 @@ Module.register("MMM-LifeCalendar", {
       // Calculate the duration in weeks between the date of birth and the event date
       this.config.date_events[i].duration_weeks = event.diff(dob, 'weeks');
     }
-
+  
     for (let i = 1; i < this.config.duration_events.length; i++) {
       this.config.duration_events[i].duration_weeks = this.config.duration_events[i].duration_years * this.config.weeksPerYear;
       if (i > 1) {
         this.config.duration_events[i].duration_weeks += this.config.duration_events[i - 1].duration_weeks;
       }
     }
-
+  
     // Create container for headers and grid
     const container = document.createElement("div");
     container.className = "grid-container-wrapper";
-
+  
     // Create grid container for year numbers and weeks
     const grid = document.createElement("div");
     grid.className = "grid-container";
     grid.style.gridTemplateColumns = `auto repeat(${this.config.weeksPerYear}, auto)`; // Include extra column for year numbers
     grid.style.gridTemplateRows = `auto auto repeat(${this.config.totalYears}, auto)`; // Include extra rows for "Weeks" header and week numbers
-
+  
     // Add "Weeks" text to the first row
     const weekHeader = document.createElement("div");
     weekHeader.className = "week-header";
@@ -97,7 +87,7 @@ Module.register("MMM-LifeCalendar", {
     weekHeader.style.gridRow = 1;
     weekHeader.style.gridColumn = "2 / span 52"; // Span all columns starting from the second column
     grid.appendChild(weekHeader);
-
+  
     // Add week numbers to the second row
     for (let i = 1; i <= this.config.weeksPerYear; i++) {
       const weekNumber = document.createElement("div");
@@ -107,7 +97,7 @@ Module.register("MMM-LifeCalendar", {
       weekNumber.style.gridColumn = i + 1; // Aligns week numbers with the columns, offset by 1
       grid.appendChild(weekNumber);
     }
-
+  
     // Add year numbers and week cells to the grid
     for (let i = 0; i < this.config.totalYears; i++) {
       const yearNumber = document.createElement("div");
@@ -116,15 +106,24 @@ Module.register("MMM-LifeCalendar", {
       yearNumber.style.gridRow = i + 3; // Aligns year numbers with the rows, offset by 2 for "Weeks" header and week numbers
       yearNumber.style.gridColumn = 1;
       grid.appendChild(yearNumber);
-
+  
       for (let j = 0; j < this.config.weeksPerYear; j++) {
         const cell = document.createElement("div");
         cell.className = "week-cell";
         cell.style.gridRow = i + 3;
         cell.style.gridColumn = j + 2;
-
-        let colorSchemeSuffix = this.config.colorScheme === "gray" ? "_gray" : "";
-
+  
+        let colorSchemeSuffix = "";
+        if (this.config.colorScheme === "gray") {
+          colorSchemeSuffix = "_gray";
+        } else if (this.config.colorScheme === "green") {
+          colorSchemeSuffix = "_green";
+        } else if (this.config.colorScheme === "beige") {
+          colorSchemeSuffix = "_beige";
+        } else if (this.config.colorScheme === "neutral") {
+          colorSchemeSuffix = "_neutral";
+        }
+  
         if (i * this.config.weeksPerYear + j < this.config.duration_events[1].duration_weeks) {
           cell.classList.add("early_years" + colorSchemeSuffix);
         } else if (i * this.config.weeksPerYear + j < this.config.duration_events[2].duration_weeks) {
@@ -137,45 +136,53 @@ Module.register("MMM-LifeCalendar", {
           cell.classList.add("college_years" + colorSchemeSuffix);
         } else if (i * this.config.weeksPerYear + j < this.config.duration_events[6].duration_weeks) {
           cell.classList.add("career_years" + colorSchemeSuffix);
-        } else if (i * this.config.weeksPerYear + j < this.config.duration_events[7].duration_weeks) {
-          cell.classList.add("retirement_years" + colorSchemeSuffix);
         }
-
+  
         if (i * this.config.weeksPerYear + j === weeksLived) {
           cell.classList.add("current_week" + colorSchemeSuffix);
         }
-
+  
         // Loop over all date_events
         this.config.date_events.forEach(event => {
           if (i * this.config.weeksPerYear + j === event.duration_weeks) {
             const tooltip = document.createElement("div");
             tooltip.className = "tooltip";
             tooltip.innerHTML = event.event_text;
-
+  
             const tooltipContainer = document.createElement("div");
             tooltipContainer.className = "tooltip-container";
             tooltipContainer.appendChild(tooltip);
             cell.appendChild(tooltipContainer);
-
+  
             // Adjusting the tooltip container to center the tooltip
             tooltipContainer.style.position = "absolute";
             tooltipContainer.style.top = "100%"; // Position the container just below the cell
             tooltipContainer.style.left = "50%";
             tooltipContainer.style.transform = "translateX(-50%)";
+
+            //Adding the icon
+            if (this.config.icons_display){
+              const icon = document.createElement("img");        
+              icon.src = "modules/MMM-LifeCalendar/Icons/" + event.icon_name;
+              icon.className="icon";
+              cell.appendChild(icon)
+            }
+
           }
         });
-
+  
         // Set the background color to black if the week number is larger than the current weeks lived
         if (i * this.config.weeksPerYear + j > weeksLived) {
           cell.style.backgroundColor = "black";
         }
-
+  
         grid.appendChild(cell);
       }
     }
-
+  
     container.appendChild(grid);
     wrapper.appendChild(container);
     return wrapper;
   },
+  
 });
